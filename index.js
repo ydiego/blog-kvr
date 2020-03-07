31901543fe8e6b189600b6f553fbfa5a7712c18f
ydiego <549863285@qq.com>
2020-03-07 20:19:38 +0800

    init koa config

---
+++ index.js
@@ -0,0 +1,38 @@
+const Koa = require('koa'),
+      path = require('path'),
+      router = require('./server/router'),
+      bodyParser = require('koa-bodyparser'),
+      static = require('koa-static'),
+      session = require('koa-session');
+
+const app = new Koa()
+
+const SESSION_CONFIG = {
+    key: 'koa:blog_session', /** (string) cookie key (default is koa:sess) */
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 86400000,
+  autoCommit: true, /** (boolean) automatically commit headers (default true) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+  renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+}
+
+app.use(session(SESSION_CONFIG, app))
+app.use(static(__dirname + '/public'))
+app.use(bodyParser())
+
+// common info
+// app.use(async (ctx, next) => {
+//     ctx.state.author = 'ydiego'
+//     await next()
+// })
+
+app
+.use(router.routes())
+.use(router.allowedMethods())
+
+app.listen(4000)
